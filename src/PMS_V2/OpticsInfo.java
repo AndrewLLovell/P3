f80a36395de10bb4c192621309813dd7acfae346
Andrew <Andrew@DESKTOP-FP4TEMJ.attlocal.net>
2023-11-04 17:31:11 -0500

    1st Commit

---
+++ src/PMS_V2/OpticsInfo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PMS_V2;
+
+/**
+ *
+ * @author Andrew
+ */
+public class OpticsInfo extends ProductInfo {
+    
+    
+    
+}
